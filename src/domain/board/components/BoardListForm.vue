e1a69299a770776106c47d9e84f56b030afe114e
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-07-14 15:57:34 +0900

    style: [유경우] 게시물 작성버튼 아이콘 추가 [SPFx]

--- src/domain/board/components/BoardListForm.vue
+++ src/domain/board/components/BoardListForm.vue
@@ -1,39 +1,30 @@
 <template>
     <div>
-      <v-layout>
-        <v-flex xs12>
-          <h3>게시물 목록</h3>
-        </v-flex>
-      </v-layout>
-      <v-layout>
-        <v-flex xs12>
-          <v-list>
-            <v-list-item-group>
-              <v-list-item v-if="!boards || (Array.isArray(boards) && boards.length === 0)" align="center">
-                <v-list-item-content>
-                  <v-list-item-title>현재 등록된 게시물이 없습니다!</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-              <v-list-item v-else v-for="board in boards" 
-                                        :key="board.id" 
-                                        @click="$router.push({name: 'BoardReadPage', 
-                                        params: {id: board.id.toString()}})">
-                <v-list-item-content>
-                  <v-list-item-title class="text-start">{{ board.id }}. {{ board.title }}</v-list-item-title>
-                  <v-list-item-subtitle class="text-start">작성자: {{ board.writer }} | 등록일자: {{ board.updateDate }}</v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-        </v-flex>
-      </v-layout>
-      <v-layout>
-        <v-flex xs12 align-end justify-end>
-          <v-btn color="primary" @click="$router.push({ name: 'BoardRegisterPage', params: {ticker: ticker.toString()}})">
+      <h3>게시물 목록</h3>
+      <v-list>
+        <v-list-item-group>
+          <v-list-item v-if="!boards || (Array.isArray(boards) && boards.length === 0)" align="center">
+            <v-list-item-content>
+              <v-list-item-title>현재 등록된 게시물이 없습니다!</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>  
+          <v-list-item v-else v-for="board in boards" 
+                                    :key="board.id" 
+                                    @click="$router.push({name: 'BoardReadPage', 
+                                    params: {id: board.id.toString()}})">
+            <v-list-item-content>
+              <v-list-item-title class="text-start">{{ board.id }}. {{ board.title }}</v-list-item-title>
+              <v-list-item-subtitle class="text-start">작성자: {{ board.writer }} | 등록일자: {{ board.updateDate }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>    
+      <div class="router-link-button" style="padding: 10px;">
+        <v-btn @click="$router.push({ name: 'BoardRegisterPage', params: { ticker: ticker.toString() } })">
+            <v-icon left> mdi-pencil </v-icon>
             게시물 작성
-          </v-btn>
-        </v-flex>
-      </v-layout>
+        </v-btn>
+        </div>
     </div>
   </template>
   
@@ -55,6 +46,13 @@
   .stockname-title {
     font-size: 24px;
     font-weight: bold;
+  }
+  
+  .router-link-button {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    margin-left: 15px;
   }
   </style>
   
