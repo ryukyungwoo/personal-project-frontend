6416de9123156ee6aa491028d6697331caa416d4
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-07-14 10:52:10 +0900

    feat: [유경우] 주식 번호로 주식 이름을 받아 올 수 있게 합니다[SPF - 20]

--- src/domain/stock/store/actions.js
+++ src/domain/stock/store/actions.js
@@ -1,5 +1,5 @@
 import { REQUEST_STOCK_LIST_TO_SPRING,
-            REQUEST_STOCK_TO_SPRING 
+            REQUEST_STOCK_TO_SPRING,
     } from './mutation-types'
 import axiosInst from '@/utility/axiosInst'
 
@@ -10,5 +10,10 @@ export default {
             commit(REQUEST_STOCK_LIST_TO_SPRING, res.data)
         })
     },
-    
+    requestStockToSpring({ commit }, ticker) {
+        return axiosInst.spring.get(`/stock/name/${ticker}`)
+        .then((res) => {
+            commit(REQUEST_STOCK_TO_SPRING, res.data)
+        })
+    }
 }
