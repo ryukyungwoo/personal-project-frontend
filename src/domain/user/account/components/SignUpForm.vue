efe525926dcac6f424dcd697061d2ebee73b3bba
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-07-29 12:01:01 +0900

    feat: [유경우] 사용자가 회원가입을 할 때 주소 기입란에서 주소를 찾을 수 있습니다[SPF-39]

--- src/domain/user/account/components/SignUpForm.vue
+++ src/domain/user/account/components/SignUpForm.vue
@@ -31,6 +31,15 @@
                                     </v-text-field>    
 
                                     <v-text-field
+                                        type="password"
+                                        v-model="confirmPassword"
+                                        label="비밀번호 확인"
+                                        :rules="confirmPassword_rule"
+                                        :disabled="false"
+                                        required
+                                    ></v-text-field>
+
+                                    <v-text-field
                                         v-model="nickname"
                                         label="닉네임"
                                         placeholder="JohnDoe"
@@ -49,13 +58,30 @@
                                     ></v-text-field>
 
                                     <v-text-field
-                                        v-model="address"
-                                        label="주소"
-                                        placeholder="서울특별시 마포구"
-                                        :rules="address_rule"
+                                        v-model="zonecode"
+                                        label="우편번호"
                                         :disabled="false"
+                                        :readonly="true"
                                         required
                                     ></v-text-field>
+
+                                    <v-text-field
+                                        v-model="roadAddress"
+                                        label="도로명 주소"
+                                        :disabled="false"
+                                        :readonly="true"
+                                        required
+                                    ></v-text-field>
+
+                                    <v-text-field
+                                        v-model="detailAddress"
+                                        label="상세 주소"
+                                        :disabled="false"
+                                        :rules="detailAddress_rule"
+                                        required
+                                    ></v-text-field>
+                                    <v-btn @click="openPostcodeSearch" color="primary">주소 검색</v-btn>
+
 
                                   </div>
                                     <v-btn type="submit" block x-large rounded :disabled="!isFormValid"
@@ -95,9 +121,14 @@ export default {
             phoneNumber_rule: [
                 (v) => !!v || "전화번호를 입력해주세요!",
                 (v) => {
-                const replaceV = v.replace(/(\s*)/g, "");
-                const pattern = /^\d{3}\d{3,4}\d{4}$/;
-                return pattern.test(replaceV) || "올바른 전화번호 형식으로 입력해주세요!(010-1234-5678)";
+                    const replaceV = v.replace(/(\s*)/g, "");
+                    const pattern = /^\d{3}\d{3,4}\d{4}$/;
+                    const isValid = pattern.test(replaceV) && replaceV.length === 11;
+
+                    return (
+                    isValid ||
+                    "올바른 전화번호 형식으로 입력해주세요!(010-1234-5678, 11자리만 가능)"
+                    );
                 },
             ],
             nickname: "",
@@ -114,25 +145,43 @@ export default {
                     );
                 },
                 ],
-            address: "",
-            address_rule: [(v) => !!v || "주소를 입력해주세요!"],
+            zonecode: "",
+            roadAddress: "",
+            detailAddress: "",
+            confirmPassword: "",
+
+            detailAddress_rule: [
+                (v) => !!v || "상세 주소를 입력해주세요",
+            ],
+            confirmPassword_rule: [(v) => v === this.password || "비밀번호가 일치하지 않습니다."],
+
         }
     },
     methods: {
         onSubmit () {
                 const { email, password, phoneNumber, nickname, address } = this
                 this.$emit("submit", { email, password, phoneNumber, nickname, address })
-        }        
+        },
+        openPostcodeSearch() {
+            new window.daum.Postcode({
+                oncomplete: (data) => {
+                console.log(data);
+                    this.zonecode = data.zonecode;
+                    this.roadAddress = data.roadAddress;
+                },
+            }).open();
+        },        
     },
     computed: {
         isFormValid() {
-        return (
-            this.email_rule.every((rule) => rule(this.email) === true) &&
-            this.password_rule.every((rule) => rule(this.password) === true) &&
-            this.phoneNumber_rule.every((rule) => rule(this.phoneNumber) === true) &&
-            this.nickname_rule.every((rule) => rule(this.nickname) === true) &&
-            this.address_rule.every((rule) => rule(this.address) === true)
-        );
+            return (
+                this.email_rule.every((rule) => rule(this.email) === true) &&
+                this.password_rule.every((rule) => rule(this.password) === true) &&
+                this.confirmPassword_rule.every((rule) => rule(this.confirmPassword) === true) &&
+                this.phoneNumber_rule.every((rule) => rule(this.phoneNumber) === true) &&
+                this.nickname_rule.every((rule) => rule(this.nickname) === true) &&
+                this.detailAddress_rule.every((rule) => rule(this.detailAddress) === true)
+            );
         },
     },
 }
